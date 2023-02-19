af7ef63379fee043e8e90b6fc3dd51e44cc18e48
Zodumo <zodumom@outlook.com>
2023-02-19 21:45:06 +0000

    completed inquirer questions

--- starter/index.js
+++ starter/index.js
@@ -4,9 +4,61 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = () => inquirer
+    .prompt([
 
-];
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username',
+        },
+        {
+            type: 'password',
+            message: 'What is your GitHub password?',
+            name: 'password',
+        },
+        {
+            type: 'password',
+            message: 'Re-enter GitHub password to confirm:',
+            name: 'confirm',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What is the description of your project?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'What is the usage of your project?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'How can your project be installed?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What can a user contribute to your project?',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'Which licence is your project licenced by?',
+            name: 'contribution'
+        },
+    ]);
+
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -14,7 +66,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+questions()
 }
 
 // function call to initialize program
